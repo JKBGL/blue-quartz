bd6d3a757d42459afd21ff608684b644cd9a35a6
JKBGL <44818773+JKBGL@users.noreply.github.com>
2022-11-27 21:07:06 +0200

    Create frontend base
    - Added index page with counts for rooms and users
    - Added basic styling
    - Added 2 api routes: /counts/users and /counts/rooms
    - Fixed max rooms comparison
    - Added font awesome

---
+++ frontend/components/room-popout.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const RoomIndexPopout = (props: { is_creating: boolean }) => {
+    // if we are creating or joining a room
+    const { is_creating } = props;
+
+    return (
+        <div className="room-popout">
+            
+        </div>
+    );
+
+};
